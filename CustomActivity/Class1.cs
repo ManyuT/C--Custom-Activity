26d9cb4067e8d5cff10ca2a66404df136dcb86d8
ManyuT <abhimanyudthite1992@gmail.com>
2019-10-10 16:56:17 +0530

    custom package and use

---
+++ CustomActivity/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Activities;
+using System.ComponentModel;
+
+namespace CompanyABC
+{
+    public class FullName: CodeActivity
+    {
+        [Category("Input")]
+        public InArgument<String> FirstName { get; set; }
+
+        [Category("Input")]
+        public InArgument<String> LastName { get; set; }
+
+        [Category("Output")]
+        public OutArgument<String> FullName1 { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var firstName = FirstName.Get(context);
+            var lastName = LastName.Get(context);
+
+            var result = firstName + " " + lastName;
+
+            FullName1.Set(context, result);
+        }
+
+    }
+}
